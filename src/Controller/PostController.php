7ae3ffcf5bbc13c91a284662954972dac6765a43
Israel Paz <israel_paz@msn.com>
2022-09-12 19:02:01 -0500

    Clase 15 guardado de datos

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,17 +3,28 @@
 namespace App\Controller;
 
 use App\Form\PostType;
+use Doctrine\ORM\EntityManager;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/crear', name: 'post_create')]
-    public function index(): Response
+    #[Route('/post/crear', name:'post_create')]
+    public function index(Request $request, EntityManager $entityManager): Response
     {
         $form = $this->createForm(PostType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+            $this->addFlash('success', 'Publicación guardada con éxito');
+            return $this->redirectToRoute('post_create');
+        }
+
 
         return $this->render('post/create.html.twig', [
             'controller_name' => 'PostController',
